2c047a55bfd9620d554ab445344426f105beadb2
Pedro Pinto <pedropinto.web@gmail.com>
2016-01-30 09:54:03 +0000

    Included JavaScript
    Angular JS included in the project, application started and controller
    connected.
    Added a few placeholder tasks that will later be replaced.

---
+++ assets/scripts.js
@@ -0,0 +1,23 @@
+// ### START NG APP ############################################################
+
+var app = angular.module("LocalList", []);
+
+
+
+// ### MAIN CONTROLLER #########################################################
+
+app.controller("TasksController", ["$scope", function($scope){
+	
+	$scope.tasks = [
+		{ description: "Add responsiveness to your layout", completed: true },
+		{ description: "List all the tasks from localstorage", completed: false },
+		{ description: "Add new tasks, inject them into localstorage as well", completed: false },
+		{ description: "Create a GitHub project to host the project", completed: true },
+		{ description: "Create usefull CSS classes", completed: true },
+		{ description: "Complete a task", completed: false },
+		{ description: "Add a way to show completed tasks", completed: true },
+		{ description: "Add keyboard shortcuts if you have time", completed: false },
+		{ description: "Dedicate some time polishing the layout and animating", completed: false }
+	];
+
+}]);
